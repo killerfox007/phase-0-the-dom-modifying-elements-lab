0ccd08671337a590f654ec69bc2e531ae9c1a656
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-06 20:25:24 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removeIt = document.getElementById('main');
+removeIt.remove();  
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = "Kaleb is the champion"
